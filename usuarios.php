6555ae032f52d11b99c0cb574b9978ace11e9e52
alvaro <alvaro@debian7-64-alumno-programacion>
2016-02-05 11:35:49 +0100

    algo

--- usuarios.php
+++ usuarios.php
@@ -1,6 +1,30 @@
-<?php
-require_once('usuarios.php.inc');
-$con = conexion_bbdd();
-trazaobj('con',$con);
-trazaobj('usuarios',lista_usuarios($con));
-?>
+<html>
+    <head>
+        <title>Aplicaci&oacute;n de ejemplo de PHP</title>
+    </head>
+    <body>
+
+        <table border=1>
+            <tr>
+                <td>ID</td><td>Nombre</td>
+            </tr>
+
+            <?php 
+            require_once('usuarios.php.inc');
+            $con = conexion_bbdd();
+            $usuarios = lista_usuarios($con);
+            foreach($usuarios as $u)
+            {
+                $link = 'modifica-usuario.php
+                echo '<tr><td>' . $u->idusuario . '</td>' ;
+                echo '<td>' . $u->nombre . "</td></tr>\n";
+            }
+            ?>
+        </table>
+        
+        <form method="post" action="crea-usuario.php">
+            <p>Nombre de usuario: <input type="text" name="nombre" /></p>
+            <p><input type="submit" /></p>
+        </form>
+    </body>
+</html>
