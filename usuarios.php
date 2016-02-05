aadabb365abb05e38153dc268847b28644e30029
alvaro <alvaro@debian7-64-alumno-programacion>
2016-02-05 12:36:13 +0100

    Mejor pinta

--- usuarios.php
+++ usuarios.php
@@ -4,39 +4,47 @@
     </head>
     <body>
 
-        <form name="formocultoparapost" action="modifica-usuario.php" method="POST">
-            <input type="hidden" name="idusuario">
-        </form>
-        <script>
-         function s(id)
-         {
-             document.formocultoparapost.idusuario.value=id;
-             document.formocultoparapost.submit();
-         }
-        </script>
 
-        <table border=1>
-            <tr>
-                <td>ID</td><td>Nombre</td>
-            </tr>
+        <div id="creacion-usuarios">
+            <h1>Creaci&oacute;n de usuario</h1>
+            <form method="post" action="crea-usuario.php">
+                <p>Nombre de usuario: <input type="text" name="nombre" /></p>
+                <p><input type="submit" /></p>
+            </form>
+        </div>
 
-            <?php 
-            require_once('usuarios.php.inc');
-            $con = conexion_bbdd();
-            $usuarios = lista_usuarios($con);
-            foreach($usuarios as $u)
-            {
+        <div id="lista-usuarios">
+            <h1>Lista de usuarios</h1>
+            <table border=1>
+                <tr>
+                    <td>ID</td><td>Nombre</td>
+                </tr>
 
-                echo '<tr><td>' . $u->idusuario . '</td>' ;
-                echo '<td><a href="#" onclick="s('.$u->idusuario.')">' . $u->nombre . "</a></td></tr>";
-                echo  "\n";
-            }
-            ?>
-        </table>
-        
-        <form method="post" action="crea-usuario.php">
-            <p>Nombre de usuario: <input type="text" name="nombre" /></p>
-            <p><input type="submit" /></p>
-        </form>
+                <?php 
+                require_once('usuarios.php.inc');
+                $con = conexion_bbdd();
+                $usuarios = lista_usuarios($con);
+                foreach($usuarios as $u)
+                {
+
+                    echo '<tr><td>' . $u->idusuario . '</td>' ;
+                    echo '<td><a href="#" onclick="s('.$u->idusuario.')">' . $u->nombre . "</a></td></tr>";
+                    echo  "\n";
+                }
+                ?>
+            </table>
+            <form name="formocultoparapost" action="modifica-usuario.php" method="POST">
+                <input type="hidden" name="idusuario">
+            </form>
+            <script>
+             function s(id)
+             {
+                 document.formocultoparapost.idusuario.value=id;
+                 document.formocultoparapost.submit();
+             }
+            </script>
+
+        </div>
+
     </body>
 </html>
